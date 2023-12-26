e4a23e0d05a355caf6a8a3cf655975d31bfd0434
J1W0N-1209 <nabook754@naver.com>
2023-12-26 22:25:07 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "BssmPortfolio2023"
---
+++ src/test/kotlin/com/bssm/bssmportfolio2023/BssmPortfolio2023ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bssm.bssmportfolio2023
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BssmPortfolio2023ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
